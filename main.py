d4d158a742b3a7e5423f8be4e920e54191e8aa97
Valera <valera.savko.2018@mail.ru>
2024-12-12 14:25:13 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
-def a(filename): # функция
-    try:
-        with open(filename, 'r', encoding='utf-8') as file: # открытие файла 
-            text = file.read() # чтение содржимого файла
-            words = text.split() # раздеение текста на слова
-            return len(words) # возврат количества слов
-    except FileNotFoundError:
-        return "Файл не найден."
-        # если файл не найден, то функция возвращает строку
-filename = 'text.txt' # имя файла 
-word = a(filename) # вызов функции
-print(f'Количество слов в файле {filename}: {word}') # вывод
+words = 0 #счетчик количества слов
+with open("text.txt", "r") as myfile: #открыть файл для чтения
+    for i in myfile:#использование цикла
+        words = len(i.split()) #подсчет количества слов
+print("Количество слов: ", words) #вывод количества слов
